2b781576914338cfa89714213242401e518c6858
Anthony Chablov <achablov@myseneca.ca>
2025-05-11 14:11:59 -0400

    docs: add useCalculator documentation

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -6,6 +6,14 @@ import ButtonGrid from "./components/Buttons/ButtonGrid";
 import useCalculator from "./hooks/useCalculator";
 
 const Calculator = () => {
+  /**
+   * useCalculator is a custom hook that manages the state and behavior of a calculator application.
+   * It provides the current expression, functions to handle button clicks, reset the calculator,
+   * and evaluate the expression.
+   *
+   * @returns {Object} - An object containing the current expression, button click handler,
+   *                     reset function, and evaluate function.
+   */
   const { expression, handleButtonClick, handleReset, onEvaluate } =
     useCalculator();
 
