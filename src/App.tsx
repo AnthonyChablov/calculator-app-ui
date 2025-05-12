b25a85be2a2a71e2bfe4a8c6b728c9ca0fb16233
Anthony Chablov <achablov@myseneca.ca>
2025-05-12 12:14:49 -0400

    feat: adjust animation speed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Calculator from "./components/Calculator/Calculator";
 import { useThemeStore } from "@/store/useThemeStore";
 import { cn } from "@/utils/utils";
-import { themeStyles } from "./store/themeVariables/themeVariables";
 
 function App() {
   // Get the theme styles from the store
@@ -11,7 +10,7 @@ function App() {
   return (
     <div
       className={cn(
-        `font-sans min-h-screen h-full ${getThemeClass("bg")} transition-all duration-500 ease-in-out`
+        `font-sans min-h-screen h-full ${getThemeClass("bg")} transition-all duration-400 ease-in-out`
       )}
     >
       <Calculator />
